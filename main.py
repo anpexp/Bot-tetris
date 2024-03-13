abe441558f293ff4a963a5300d58fc1eead299d3
anpexp <anpovedab@unal.edu.co>
2024-03-13 16:55:15 -0500

    algunas consideraciones

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ import pyautogui
 #Es más eficiente hacer los cálculos con cuantas fichas?
 #Reducir el problema a llenar huecos?
 
+
+#una propuesta de estrategia es que en la matriz se sumen los pesos de las filas y se promedie la altura,
+#la jugada que reduce más el promedio de la matriz es la jugada más efectiva
+
+#¿De qué forma representamos la posición y forma de una ficha de colocarse en un sitio?
+
 class jugador:
     def __init__ (self, hold, line, map):
         self.line = self.getline()
         self.hold = "" #Las fichas se pueden definir por letras (l,j,i,o,s,z,t)
+        self.map = map #Este debería ser una matriz de unos y ceros
 
     
     def getline():
@@ -27,7 +34,9 @@ class jugador:
         return frame
 
     def search():
-    #Aquí realiza la busqueda de la última ficha agregada
+        #Aquí realiza la busqueda de la última ficha agregada
 
+    def estrategy();
+        #Aquí hay que hacer el algoritmo que decide como jugar
 
 keyboard.press_and_release('alt+tab')
